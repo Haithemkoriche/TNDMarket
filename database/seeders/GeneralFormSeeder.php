66772ec70017720951906d8ea44f22c59c654949
haithemkoriche <koriche@tabiblib-services.com>
2025-07-09 20:10:24 +0100

    feat: Add general fields data and update GeneralFormSeeder

--- database/seeders/GeneralFormSeeder.php
+++ database/seeders/GeneralFormSeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\FormType;
 
 class GeneralFormSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        FormType::create([
+            'name' => 'Bilan General',
+            'description' => 'Évaluation General',
+            'fields' => json_decode(file_get_contents(database_path('data/general_fields.json')), true),
+            'is_active' => true,
+        ]);
     }
 }
